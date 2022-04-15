2792389918b11e7eecd7cb2220c0b27851d30ec7
Parabeac-Nest <admin@parabeac.com>
2022-04-15 10:38:15 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/main_widgetbook.g.dart
@@ -0,0 +1,74 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+    import 'package:widgetbook/widgetbook.dart';
+    import 'package:flutter/material.dart';
+    import 'package:test12/widgets/checkbox/checkbox.g.dart';
+
+
+    void main() {
+      runApp(const MyApp());
+    }
+
+    class MyApp extends StatelessWidget {
+      const MyApp({Key? key}) : super(key: key);
+
+      @override
+      Widget build(BuildContext context){
+        return Widgetbook(
+          themes: [
+            WidgetbookTheme(name: 'Light', data: ThemeData.light()),
+          ],
+          devices: const [
+            Apple.iPhone11ProMax,
+            Samsung.s10,
+          ],
+          categories: [
+                  WidgetbookCategory(
+        name: 'Parabeac-Generated',
+        folders: [
+      WidgetbookFolder(
+        name: 'checkbox',
+        widgets: [
+      WidgetbookWidget(
+        name: 'Checkbox',
+        useCases: [
+      WidgetbookUseCase(
+        name: 'Checkbox',
+        builder: (context) => Center(child:       SizedBox(
+        width: 16.000,height: 16.000,
+        child: LayoutBuilder( 
+  builder: (context, constraints) {
+    return Checkbox(
+constraints,
+)
+;
+}
+),
+      ),
+    ),
+      ),
+    
+],
+
+      )
+    
+],
+
+      ),
+    
+],
+
+        
+      )
+    ,
+          ],
+          appInfo: AppInfo(name: 'MyApp'),
+        );
+      }
+    }
+    
